18ab1f2668176fb20f85a93cd10c406c73dd7fbe
jhon wayner <120293229+joyiger07@users.noreply.github.com>
2023-04-19 17:44:32 -0500

    aplicando navbar,footer y section
    listo

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+
+
+export default function Navbar() {
+    return <div className={Navbar}>
+        <img src="" />
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+        </ul>
+      
+        <button>Call to Action</button>
+    </div>
+}
